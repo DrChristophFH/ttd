daba4eab75acd9901882c37f2e6cc34fb0d15c6a
zack <zack@gamesbrown.com>
2024-08-27 16:19:42 -0400

    multiclient server functionality with messages

--- server/Client.cs
+++ server/Client.cs
@@ -22,11 +22,16 @@ namespace Client
             IPEndPoint localEndPoint = new IPEndPoint(client.serverAddr, client.serverPort);
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
         ProtocolType.Tcp );
+        byte[] bytes = new byte[1024];
         Connect(localEndPoint,s);
    
         while (true)
         {
+            Console.WriteLine("Waiting for message...");
+            
             Console.ReadLine();
+            int byteCount = s.Receive(bytes, 0, bytes.Length,SocketFlags.None);
+            Console.WriteLine(Encoding.UTF8.GetString(bytes, 0, byteCount));
 
         }
         }
--- server/Server.cs
+++ server/Server.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 
 
@@ -11,11 +12,22 @@ namespace Server
 {
     class Server
     {
+        static Dictionary<int, Socket> activeConnections = new Dictionary<int, Socket>();
+        public static int connectionID = 0;
+        int addConnection(Socket s,Dictionary<int, Socket> activeConnections, int connectionID)
+        {
+            activeConnections.Add(connectionID,s);
+            connectionID++;
+            return connectionID;
+        }
         public IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
         public int port = 55555;
         public async Task AcceptAsyncConnection(Socket s)
         {
-            var handler = await s.AcceptAsync();
+            Socket handler = await s.AcceptAsync();
+            Console.WriteLine("Connection accepted!");
+           
+            connectionID = addConnection(handler,activeConnections,connectionID);
 
         }
         
@@ -24,22 +36,44 @@ namespace Server
         Server serverHost = new Server();
         IPEndPoint localEndPoint = new IPEndPoint(serverHost.ipAddr, serverHost.port);
        
-        //Console.WriteLine(serverHost.ipAddr);
-        //Console.WriteLine(serverHost.ipAddr.GetType());
-        //Console.WriteLine(localEndPoint);
-        //Console.WriteLine(localEndPoint.GetType());
+
         Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
         ProtocolType.Tcp );
-        //Console.WriteLine(s);
-        //Console.WriteLine(s.GetType());
+
         s.Bind(localEndPoint);
         s.Listen(5);
-        var handler = serverHost.AcceptAsyncConnection(s);
+        byte[] buffer = new byte[1024];
+        int offset =0;
+
+        
 
         while(true)
-        {
+        {   
+
             Console.WriteLine("Waiting for incoming connection...");
+            var handler = serverHost.AcceptAsyncConnection(s);
             Console.ReadLine();
+            if (activeConnections.Count != 0)
+            {
+                Console.WriteLine("Type 'send' or 's' to send a message to a client.");
+                string input = Console.ReadLine();
+                
+                if (input == "send")
+                {
+                    Console.WriteLine("Select a client to send your message to.");
+                    string clientId = Console.ReadLine();
+                    Console.WriteLine("Type the message you would like to send.");
+                    string msg = Console.ReadLine();
+                    Console.WriteLine("The message "+msg+" was sent to client: " + clientId);
+                    byte[] byteMsg = Encoding.UTF8.GetBytes(msg);
+                    int byteCount = activeConnections[Int32.Parse(clientId)].Send(byteMsg, 0, byteMsg.Length,SocketFlags.None);
+                    if (byteCount > 0 )
+                        Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, byteCount));
+                }
+            }
+
+
+            
         }
     }
    
